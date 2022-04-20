ec2331d710dbae5dce9cdb323b5792263c3a8edd
Alexandra <38242434+Ales12@users.noreply.github.com>
2022-04-20 15:49:32 +0200

    Add files via upload

---
+++ inc/languages/deutsch_du/canons.lang.php
@@ -0,0 +1,56 @@
+<?php
+// alle Canons
+$l['canons_welcome']="Willkommen bei den Canons";
+$l['canon_age'] = "Alter";
+$l['canon_gender'] = "Geschlecht";
+$l['canon_def'] = "Zugehörigkeit";
+$l['canon_group'] = "Gruppe";
+$l['canon_else'] = "weitere Informationen";
+$l['canon_between'] = " || ";
+$l['canon_lookslike'] = "Sieht aus wie ";
+$l['canon_delete'] = "<i class='fa-solid fa-trash-can' title='Canon löschen'></i>";
+$l['canon_edit'] = "<i class='fa-solid fa-file-pen' title='Canon editieren'></i>";
+
+$l['canon_reserv'] = "Canon reservieren";
+$l['canon_reserv_name'] = "Auf welchen Namen magst du reservieren?";
+$l['canon_submit_reserv'] = "Canon reservieren lassen";
+$l['canon_delete_reserv'] = "<i class='fa-solid fa-user-minus' title='Reservierung löschen'></i>";
+$l['canon_taken'] = "<i class='fa-solid fa-file-circle-plus' title='Canon als vergeben markieren'></i>";
+$l['canon_taken_off'] = "<i class='fa-solid fa-file-circle-minus' title='Canon wieder freigeben'></i>";
+$l['canon_submit_taken'] = "Canon einen Account zuweisen";
+
+// Navigation
+$l['canons_nav_all'] = "Canons";
+$l['canons_nav_add'] = "Canons hinzufügen";
+
+// Canon hinzufügen
+$l['canons_add'] = "Einen neuen Canon hinzufügen";
+$l['canons_name'] = "Name des Charakters";
+$l['canons_age'] = "Alter des Charakters";
+$l['canons_group'] = "Gruppe des Charakters";
+$l['canons_else'] = "Sonstige Informationen";
+$l['canons_else_desc'] = "Hier kannst du z.B. den Beruf oder andere weitere Informationen einfügen.";
+$l['canons_def'] = "Zugehörigkeit";
+$l['canons_def_desc'] = "Welcher genauen Gruppierung (Hogwarts, Orden des Phönix etc.) gehört der Charakter an?";
+$l['canons_pic'] = "Bild des Charakter";
+$l['canons_pic_desc'] = "Füge hier den Link fürs Bild ein. Das Bild sollte 120x150px sein.";
+$l['canons_desc'] = "Charakterbeschreibung";
+$l['canons_submit'] = "Canon hinzufügen";
+$l['canons_avatar'] = "Avaterpersonverschlag";
+$l['canons_gender'] = "Geschlecht des Charakters";
+$l['canons_blood'] = "Blutstatus des Charakters";
+
+//Canon bearbeiten
+$l['canon_submitedit'] = "Änderungen speichern";
+$l['canon_edit_form'] = "Canon bearbeiten";
+
+// global Alerts
+$l['canon_alert'] = "<a href='modcp.php?action=canons'>Es gibt neue Canoneinträge</a>";
+
+// Modcp
+$l['canon_modcp_nav'] = "Canons verwalten";
+$l['canon_modcp'] = "neue Canoneinträge verwalten";
+$l['canon_accept'] = "Canon annehmen";
+$l['canon_reject'] = "Canon ablehnen";
+$l['canon_reason'] = "Begründung für die Ablehnung";
+$l['canon_reject_pm'] = "Dein Canon wurde abgelehnt";
